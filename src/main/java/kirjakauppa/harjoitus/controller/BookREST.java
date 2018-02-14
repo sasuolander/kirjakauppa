0619ed2f2909e47c985d6185d6d1ce743038e50e
sasu <sasuolander@yahoo.com>
2018-02-14 12:22:23 +0200

    bootstrap

---
+++ src/main/java/kirjakauppa/harjoitus/controller/BookREST.java
@@ -0,0 +1,31 @@
+package kirjakauppa.harjoitus.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import kirjakauppa.harjoitus.object.Kirja;
+import kirjakauppa.harjoitus.repository.KirjaRepo;;
+
+@RestController
+@RequestMapping(value="api")
+public class BookREST {
+	
+	@Autowired
+	KirjaRepo repo;
+	
+	@RequestMapping(value="kirjat")
+	public @ResponseBody List<Kirja> books (){
+		return (List<Kirja>)repo.findAll();
+	}
+	
+	@RequestMapping(value="kirja/{id}")
+	public @ResponseBody Kirja book (@PathVariable(value="id")Long id){
+		return repo.findOne(id);
+	}
+	
+}
---
+++ target/classes/data.sql
@@ -0,0 +1,2 @@
+insert into Kirja (id,author,isbn,price,title,year) values(1,'tim','tim',0.20,'tim',2001
+);
